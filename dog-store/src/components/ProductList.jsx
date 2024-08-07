b340c3a1004eeef50e31da638cf7e08833432ddc
sarwoodford <sarawoodford6@gmail.com>
2024-08-07 14:14:39 -0230

    completing product list component

---
+++ dog-store/src/components/ProductList.jsx
@@ -0,0 +1,30 @@
+// Product List Component for Dog Store React Application
+// submission date: august 12, 2024
+// author: Sara Woodford
+
+import React, { useState, useEffect } from 'react';
+import { getProducts } from '../api/api';
+import '../styles/productList.css';
+
+const ProductList = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const productInfo = getProducts();
+        setProducts(productInfo);
+    }, []);
+
+    return(
+        <div className = 'productList'>
+            {products.map((product) => (
+                <div key={product.id} className = 'productItemCont'>
+                    <h4>{product.product}</h4>
+                    <p>{product.description}</p>
+                    <p>${product.price}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ProductList;
